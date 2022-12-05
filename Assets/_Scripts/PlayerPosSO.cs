8be67cd9a96f5071ccdd5b25822b0d40f31a9cb6
voanh <chihuahua.iu.sica@gmail.com>
2022-12-05 21:24:36 +0700

    update load room

---
+++ Assets/_Scripts/PlayerPosSO.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPlayerPos", menuName = "Player/New Player Pos")]
+public class PlayerPosSO : ScriptableObject
+{
+    [SerializeField]
+    private int pos;
+    
+    public int Pos
+    {
+        get => pos;
+        set
+        {
+            pos = value;
+            OnPosChange?.Invoke(pos);
+        }
+    }
+
+    public event Action<int> OnPosChange;
+}
---
+++ Assets/_Scripts/RoomCollider.cs
@@ -0,0 +1,20 @@
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class RoomCollider : MonoBehaviour
+{
+    private int roomNumber;
+    [SerializeField] private PlayerPosSO playerPos;
+
+    private void OnEnable()
+    {
+        roomNumber = int.Parse(Regex.Match(gameObject.name, @"\d+").Value);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            playerPos.Pos = roomNumber;
+    }
+}
