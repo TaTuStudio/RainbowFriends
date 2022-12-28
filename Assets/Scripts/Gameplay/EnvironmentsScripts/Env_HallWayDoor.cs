45dd96d73a8d7b124e6d42a4e1e0fc6486f09ac3
eliteforcevn <eliteforcevn@Gmail.com>
2022-12-28 18:14:57 +0700

    Bổ xung cập nhât A* Gridlayer sau khi cửa mở xong

--- Assets/Scripts/Gameplay/EnvironmentsScripts/Env_HallWayDoor.cs
+++ Assets/Scripts/Gameplay/EnvironmentsScripts/Env_HallWayDoor.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Pathfinding;
 
 public class Env_HallWayDoor : MonoBehaviour
 {
     [Tooltip("Rotate -90 degree")]
     public Transform[] doors1;
-    
+
     [Tooltip("Rotate 90 degree")]
     public Transform[] doors2;
 
@@ -27,21 +28,28 @@ public class Env_HallWayDoor : MonoBehaviour
 
     void _OpenDoorDelay()
     {
-        if(curOpenDoorDelay >= 0f)
+        if (curOpenDoorDelay >= 0f)
         {
             curOpenDoorDelay -= Time.deltaTime;
 
-            if(curOpenDoorDelay < 0f)
+            if (curOpenDoorDelay < 0f)
             {
                 foreach (var door in doors1)
                 {
-                    door.DOLocalRotate(new Vector3(0, 0, -90), 1f);
+                    door.DOLocalRotate(new Vector3(0, 0, -90), 1f).OnComplete(() => _UpdateAStarGridLayer(door));
                 }
                 foreach (var door in doors2)
                 {
-                    door.DOLocalRotate(new Vector3(0, 0, 90), 1f);
+                    door.DOLocalRotate(new Vector3(0, 0, 90), 1f).OnComplete(() => _UpdateAStarGridLayer(door));
                 }
             }
         }
+    }
+
+    private void _UpdateAStarGridLayer(Transform door)
+    {
+        var guo = new GraphUpdateObject(door.GetComponent<Collider>().bounds);
+
+        AstarPath.active.UpdateGraphs(guo);
     }
 }
